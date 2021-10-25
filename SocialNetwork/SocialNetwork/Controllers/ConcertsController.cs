5e9185cded6cd937885643f213bc19845356d264
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-10-25 13:12:24 +0200

    Impleming Create action for Concert Entity.

--- SocialNetwork/SocialNetwork/Controllers/ConcertsController.cs
+++ SocialNetwork/SocialNetwork/Controllers/ConcertsController.cs
@@ -1,5 +1,7 @@
-﻿using SocialNetwork.Models;
+﻿using Microsoft.AspNet.Identity;
+using SocialNetwork.Models;
 using SocialNetwork.ViewModels;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -15,6 +17,8 @@ namespace SocialNetwork.Controllers
 			_dbContext = new ApplicationDbContext();
 		}
 
+
+		[Authorize]
 		public ActionResult Create()
 		{
 			var viewModel = new ConcertFormViewModel()
@@ -23,6 +27,28 @@ namespace SocialNetwork.Controllers
 			};
 
 			return View(viewModel);
+		}
+
+		[Authorize]
+		[HttpPost]
+		public ActionResult Create(ConcertFormViewModel viewModel)
+		{
+			var artistId = User.Identity.GetUserId();
+			var artist = _dbContext.Users.Single(u => u.Id == artistId);
+			var genre = _dbContext.Genres.Single(g => g.Id == viewModel.Genre);
+
+			var concert = new Concert
+			{
+				Artist = artist,
+				DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+				Genre = genre,
+				Venue = viewModel.Venue
+			};
+
+			_dbContext.Concerts.Add(concert);
+			_dbContext.SaveChanges();
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
