3ae1c7ac38f62ed982c74e565c8cae839d9367f1
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-10-25 11:47:09 +0200

    Creating Domain Classes, Concert and Genre

---
+++ SocialNetwork/SocialNetwork/Models/Concert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SocialNetwork.Models
+{
+	public class Concert
+	{
+		public int Id { get; set; }
+		public ApplicationUser Artist { get; set; }
+		public DateTime DateTime { get; set; }
+		public string Venue { get; set; }
+		public Genre Genre { get; set; }
+
+	}
+}
