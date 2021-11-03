72fcb02e1b2cfb6a6134c6ad28a586c89313aee2
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-11-03 15:04:41 +0100

    Formatting date/time using MomentJs

--- SocialNetwork/SocialNetwork/App_Start/BundleConfig.cs
+++ SocialNetwork/SocialNetwork/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace SocialNetwork
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js",
-						"~/Scripts/underscore-umd-min.js"));
+						"~/Scripts/underscore-umd-min.js",
+						"~/Scripts/moment.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
